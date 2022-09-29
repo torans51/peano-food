ee404710c7a611e6f500422d1babdadfe7526541
Alessandro <alessandro>
2022-09-29 13:43:09 +0200

    fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
-  return (
-    <div class="bg-black text-white">
-      test
-    </div>
-  );
+  return <div class="bg-black text-white">test</div>;
 };
 
 export default App;
