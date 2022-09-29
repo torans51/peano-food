b72c710bdb7616b3da45eb44f61ada734e693c35
Alessandro <alessandro>
2022-09-29 13:05:08 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    'globals': {
+        module: true
+    },
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:solid/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint',
+        'solid'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
