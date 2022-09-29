2b24521ec3a17989ab768c6b102e8f6ff4e4c602
Alessandro <alessandro>
2022-09-29 13:42:42 +0200

    add Menu component

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const Menu: Component = () => {
+  return (
+    <div class="bg-black text-white">
+      menu
+    </div>
+  );
+};
+
+export default Menu;
