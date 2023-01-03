6df173d2755e85580b9ec7b6c8ffce42e77dd9ce
torans51 <>
2023-01-03 18:05:59 +0100

    asjad

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,23 @@ import { Header, Footer } from 'src/components';
 const App: Component = () => {
   return (
     <OrderProvider>
-      <div class="flex justify-center">
-        <div class="flex-1 h-screen max-w-md bg-neutral-50 flex flex-col">
+      <div class="h-screen flex justify-center">
+        <div class="flex-1 h-full max-w-md bg-neutral-50 flex flex-col">
           <Header />
           <div class="flex-1 overflow-y-auto">
             <MenuPage />
-            <Toaster
-              position="bottom-center"
-              containerClassName="mb-12"
-              toastOptions={{
-                duration: 1000,
-                className:
-                  'border border-t-blue-400 border-l-blue-400 border-b-purple-400 border-r-purple-400 shadow-xl shadow-blue-400',
-              }}
-            />
           </div>
           <Footer />
         </div>
+        <Toaster
+          position="bottom-center"
+          containerClassName="mb-12"
+          toastOptions={{
+            duration: 1000,
+            className:
+              'border border-t-blue-400 border-l-blue-400 border-b-purple-400 border-r-purple-400 shadow-xl shadow-blue-400',
+          }}
+        />
       </div>
     </OrderProvider>
   );
